2efb94b694606c8e04956ece27a8d9e970b5fca9
Heejin Im <dlagmlwls39@naver.com>
2026-02-25 09:51:51 +0900

    연습 문제: 구구단
    Implement multiplication table function and call it.

---
+++ ch03/multi.py
@@ -0,0 +1,7 @@
+def multi():
+    for i in range(2, 10):
+        for j in range(1, 10):
+            # print('{0} * {1} = {2}'.format(i, j, i * j))
+            print(f'{i} * {j} = {i * j:2d}')
+
+multi()
